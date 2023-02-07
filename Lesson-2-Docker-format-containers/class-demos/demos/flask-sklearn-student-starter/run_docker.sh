43734a82dac0b9f209533d68a2694b344b05056c
EC2 Default User <ec2-user@ip-172-31-91-79.ec2.internal>
2023-02-07 11:29:50 +0000

    build docker image

--- Lesson-2-Docker-format-containers/class-demos/demos/flask-sklearn-student-starter/run_docker.sh
+++ Lesson-2-Docker-format-containers/class-demos/demos/flask-sklearn-student-starter/run_docker.sh
@@ -2,11 +2,11 @@
 
 ## Complete the following steps to get Docker running locally
 
-# Step 1:
 # Build image
+docker build --tag=localdemo
 
-# Step 2: 
 # List docker images
+docker image ls
 
-# Step 3: 
 # Run flask app
+docker run -it localdemo bash
